ddeddcc4e8d97a969c33022b6db6e5159a987411
danelkhen <danelkhen@gmail.com>
2016-11-24 14:54:05 +0100

    g

--- client/src/app.module.ts
+++ client/src/app.module.ts
@@ -1,10 +1,18 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
+import { AppComponent } from './app.component';
+import { BrowserComponent } from './browser.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  //{ path: '', component: HomeComponent },
+  { path: '**', component: BrowserComponent }
+];
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+    imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
+    declarations: [AppComponent, BrowserComponent],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
