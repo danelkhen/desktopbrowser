0656cdc8269e7385759822fccba68ba4f12de073
danelkhen <danelkhen@gmail.com>
2016-12-08 17:43:03 +0100

    g

---
+++ server/src/db2.ts
@@ -0,0 +1,50 @@
+import "reflect-metadata";
+import { Table, Column, PrimaryGeneratedColumn, PrimaryColumn, Connection, ConnectionOptions, createConnection, Repository } from "typeorm";
+import { ColumnTypes } from "typeorm/metadata/types/ColumnTypes";
+
+@Table()
+export class ByFilename {
+
+    @PrimaryColumn()
+    key: string;
+
+    @Column(ColumnTypes.SIMPLE_ARRAY)
+    selectedFiles?: string[];
+}
+
+
+export class Db {
+    connection: Connection;
+    connectionOptions: ConnectionOptions = {
+        driver: {
+            type: "sqlite",
+            host: "localhost",
+            //port: 3306,
+            //username: "root",
+            //password: "admin",
+            database: "test",
+            storage: "test.sqlite",
+        },
+        entities: [ByFilename],
+        autoSchemaSync: true,
+    };
+    init(): Promise<any> {
+        return createConnection(this.connectionOptions).then(connection => this.connection = connection);
+    }
+    get byFilename(): Repository<ByFilename> { return this.connection == null ? null : this.connection.getRepository(ByFilename); }
+}
+
+
+function test() {
+    //console.log(ColumnTypes);
+    //console.log(ColumnTypes.SIMPLE_ARRAY);
+    //return;
+    let db = new Db();
+    db.init().then(() => {
+        let x = new ByFilename();
+        x.key = "ggg";
+        x.selectedFiles = ["aaa", "bbb", "ccc"];
+        db.byFilename.persist(x).then(() => console.log("Done"));
+    });
+}
+//test();
--- server/src/testtypeorm.ts
+++ server/src/testtypeorm.ts
@@ -1,5 +1,4 @@
 import "reflect-metadata";
-import "reflect-metadata";
 import { createConnection } from "typeorm";
 
 import { Table, Column, PrimaryGeneratedColumn } from "typeorm";
