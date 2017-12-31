dc116f96c4a987ce9bd92d08d7eed8a3c7537eba
danelkhen <danelkhen@gmail.com>
2017-12-31 17:20:07 +0100

    continue

--- client/src/websocket.ts
+++ client/src/websocket.ts
@@ -1,46 +1,65 @@
-﻿let rxq: string[] = [];
-function main() {
-    let url = location.href.replace(/^https|http/, "ws");
-    let webSocket = new WebSocket(url, ["protocolOne", "protocolTwo"]);
-    webSocket.addEventListener("message", e => {
-        console.log(e.data);
-        rxq.push(e.data);
-    });
-    // Connection opened
-    webSocket.addEventListener('open', e => {
-        //socket.send('Hello Server!');
-    });
-}
+﻿//let rxq: string[] = [];
+//let webSocket: WebSocket;
+//function main() {
+//    let url = location.href.replace(/^https|http/, "ws");
+//    webSocket = new WebSocket(url, ["protocolOne", "protocolTwo"]);
+//    webSocket.addEventListener("message", e => {
+//        console.log(e.data);
+//        rxq.push(e.data);
+//    });
+//    // Connection opened
+//    webSocket.addEventListener('open', e => {
+//        //socket.send('Hello Server!');
+//    });
+//}
 
-function send(cmd: string) {
+//async function* iterateEvent(target: EventTarget, name: string) {
 
-}
+//    target.addEventListener(name, e => {
 
-function receive(res: string) {
-    if (res == "[") {
+//    });
 
-    }
-    else if (res.endsWith(",")) {
+//    let item = await next();
+//    yield return item;
+//}
+//function send(cmd: string) {
+//    webSocket.send(cmd);
+//    let result: any;
 
-    }
-    else if (res == "]") {
+//    webSocket.addEventListener("message", e => {
 
-    }
-    else {
+//        let data = e.data;
+//        if (data == "[") {
+//            result = [];
+//        }
+//        else if (data.endsWith(",")) {
+//            let item = JSON.parse(data.substr(0, data.length - 1));
+//            result.push(item);
+//        }
+//        else if (data == "]") {
+//            done();
+//        }
+//        else {
+//            result = JSON.parse(data);
+//            done();
+//        }
+//    });
 
-    }
+//}
 
-}
+//function receive(data: string) {
 
-export interface Request {
+//}
+
+//export interface Request {
 
 
-}
+//}
 
-export interface Response<T> {
-    error: any;
-    result: T;
-    resultType: "iterable" | "object";
-    isComplete: boolean;
-}
+//export interface Response<T> {
+//    error: any;
+//    result: T;
+//    resultType: "iterable" | "object";
+//    isComplete: boolean;
+//}
 
