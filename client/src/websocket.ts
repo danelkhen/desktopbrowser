68232e94f60503842afb6aa96f62f37be5dead32
danelkhen <danelkhen@gmail.com>
2017-12-31 18:23:33 +0100

    continue

--- client/src/websocket.ts
+++ client/src/websocket.ts
@@ -1,65 +1,75 @@
-﻿//let rxq: string[] = [];
-//let webSocket: WebSocket;
-//function main() {
-//    let url = location.href.replace(/^https|http/, "ws");
-//    webSocket = new WebSocket(url, ["protocolOne", "protocolTwo"]);
-//    webSocket.addEventListener("message", e => {
-//        console.log(e.data);
-//        rxq.push(e.data);
-//    });
-//    // Connection opened
-//    webSocket.addEventListener('open', e => {
-//        //socket.send('Hello Server!');
-//    });
-//}
+﻿let rxq: string[] = [];
+let webSocket: WebSocket;
+function main() {
+    let url = location.href.replace(/^https|http/, "ws");
+    webSocket = new WebSocket(url, ["protocolOne", "protocolTwo"]);
+    webSocket.addEventListener("message", e => {
+        console.log(e.data);
+        rxq.push(e.data);
+    });
+    // Connection opened
+    webSocket.addEventListener('open', e => {
+        //socket.send('Hello Server!');
+    });
+}
 
-//async function* iterateEvent(target: EventTarget, name: string) {
+async function* iterateEvent(target: EventTarget, name: string): AsyncIterableIterator<any> {
+    let list: Promise<any>[] = [];
+    let pResolve: Function;
+    let p = new Promise((resolve, reject) => pResolve = resolve);
+    list.push(p);
+    target.addEventListener(name, e => {
+        pResolve(e);
+        p = new Promise((resolve, reject) => pResolve = resolve);
+        list.push(p);
+    });
 
-//    target.addEventListener(name, e => {
-
-//    });
-
-//    let item = await next();
-//    yield return item;
-//}
-//function send(cmd: string) {
-//    webSocket.send(cmd);
-//    let result: any;
-
-//    webSocket.addEventListener("message", e => {
-
-//        let data = e.data;
-//        if (data == "[") {
-//            result = [];
-//        }
-//        else if (data.endsWith(",")) {
-//            let item = JSON.parse(data.substr(0, data.length - 1));
-//            result.push(item);
-//        }
-//        else if (data == "]") {
-//            done();
-//        }
-//        else {
-//            result = JSON.parse(data);
-//            done();
-//        }
-//    });
-
-//}
-
-//function receive(data: string) {
-
-//}
-
-//export interface Request {
+    for (let p of list) {
+        let item = await p;
+        yield item;
+    }
+}
 
 
-//}
 
-//export interface Response<T> {
-//    error: any;
-//    result: T;
-//    resultType: "iterable" | "object";
-//    isComplete: boolean;
-//}
+function send(cmd: string) {
+    webSocket.send(cmd);
+    let result: any;
+
+    webSocket.addEventListener("message", e => {
+
+        let data = e.data;
+        if (data == "[") {
+            result = [];
+        }
+        else if (data.endsWith(",")) {
+            let item = JSON.parse(data.substr(0, data.length - 1));
+            result.push(item);
+        }
+        else if (data == "]") {
+            //done();
+        }
+        else {
+            result = JSON.parse(data);
+            // done();
+        }
+    });
+
+}
+
+function receive(data: string) {
+
+}
+
+export interface Request {
+
+
+}
+
+export interface Response<T> {
+    error: any;
+    result: T;
+    resultType: "iterable" | "object";
+    isComplete: boolean;
+}
 
