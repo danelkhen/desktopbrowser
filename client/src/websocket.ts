74a6815ac21efbdebe1c3ed55eba58defd789493
danelkhen <danelkhen@gmail.com>
2017-12-31 13:20:55 +0100

    begin websocket data streaming api

---
+++ client/src/websocket.ts
@@ -0,0 +1,46 @@
+﻿let rxq: string[] = [];
+function main() {
+    let url = location.href.replace(/^https|http/, "ws");
+    let webSocket = new WebSocket(url, ["protocolOne", "protocolTwo"]);
+    webSocket.addEventListener("message", e => {
+        console.log(e.data);
+        rxq.push(e.data);
+    });
+    // Connection opened
+    webSocket.addEventListener('open', e => {
+        //socket.send('Hello Server!');
+    });
+}
+
+function send(cmd: string) {
+
+}
+
+function receive(res: string) {
+    if (res == "[") {
+
+    }
+    else if (res.endsWith(",")) {
+
+    }
+    else if (res == "]") {
+
+    }
+    else {
+
+    }
+
+}
+
+export interface Request {
+
+
+}
+
+export interface Response<T> {
+    error: any;
+    result: T;
+    resultType: "iterable" | "object";
+    isComplete: boolean;
+}
+
