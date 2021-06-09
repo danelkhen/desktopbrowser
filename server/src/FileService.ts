5a4b63be87b28524699f4954400d25ec83413fb2
Dan-el Khen <danelkhen@gmail.com>
2021-06-09 14:24:12 +0300

    cleanups

--- server/src/FileService.ts
+++ server/src/FileService.ts
@@ -1,10 +1,10 @@
 ﻿import * as child_process from "child_process"
 import open from "open"
 import * as os from "os"
-import trash from "trash"
+import trash2 from "trash"
 import { equalsIgnoreCase } from "../../shared/src"
 import * as C from "../../shared/src/contracts"
-import { File, FileRelativesInfo, ListFilesRequest, ListFilesResponse, PathRequest } from "../../shared/src/contracts"
+import { FileRelativesInfo, ListFilesResponse } from "../../shared/src/contracts"
 import {
     ApplyCaching,
     ApplyPaging,
@@ -23,121 +23,110 @@ export function isWindows() {
     return os.platform() == "win32"
 }
 
-// const ListFiles:C.FileService["ListFiles"] = async res => {
-
-// }
-export function createFileService2(): Omit<
-    C.FileService,
-    "getAllFilesMetadata" | "saveFileMetadata" | "deleteFileMetadata"
-> {
-    const _this: Omit<C.FileService, "getAllFilesMetadata" | "saveFileMetadata" | "deleteFileMetadata"> = {
-        async ListFiles(req: ListFilesRequest): Promise<ListFilesResponse> {
-            //if (req.Path == null) {
-            //    return {
-            //        Relatives: { ParentFolder: null, NextSibling: null, PreviousSibling: null },
-            //        File: { Name: "", IsFolder: true },
-            //        Files: this.GetHomeFiles(),
-            //    };
-            //}
-            if (req.Path != null && req.Path.endsWith(":")) {
-                req.Path += "\\"
-            }
-            let res: ListFilesResponse = {
-                Relatives: await this.GetFileRelatives(req.Path!),
-                File: await this.GetFile({ Path: req.Path! }),
-                // Files: null,
-            }
-            if (res.File!.IsFolder) {
-                res.Files = await this.GetFiles(req) //.toArray();
-            }
-            return res
-        },
-
-        async GetFiles(req: ListFilesRequest): Promise<File[]> {
-            if (req.HideFiles && req.HideFolders) return []
-            //else if (!req.MixFilesAndFolders && !req.HideFiles && !req.HideFolders && !req.IsRecursive) {
-            //   const folders = GetFileAndOrFolders(req.Path, req.SearchPattern, req.IsRecursive, false, true);
-            //   const files = GetFileAndOrFolders(req.Path, req.SearchPattern, req.IsRecursive, true, false);
-            //    folders = this.ApplyRequest(folders, req);
-            //    files = this.ApplyRequest(files, req);
-            //   const all = folders.concat(files);
-            //    all = this.ApplyPaging(all, req);
-            //    all = this.ApplyCaching(all);
-            //    return all;
-            //}
-            //else {
-            let files = await GetFileAndOrFolders({
-                path: req.Path!,
-                searchPattern: req.SearchPattern,
-                recursive: req.IsRecursive,
-                files: !req.HideFiles,
-                folders: !req.HideFolders,
-            })
-            files = await ApplyRequest(files, req)
-            files = ApplyPaging(files, req)
-            files = ApplyCaching(files)
-            return files
-            //}
-        },
-
-        async GetFileRelatives(path: string): Promise<FileRelativesInfo> {
-            if (!path) return {}
-            const pathInfo = new PathInfo(path)
-            const info: FileRelativesInfo = {}
-            info.ParentFolder = await this.GetFile({ Path: pathInfo.ParentPath.Value })
-            let xxx = await GetFileAndOrFolders({ path: info.ParentFolder.Path!, files: false, folders: true })
-            const parentFiles = xxx.filter(t => t.IsFolder)[orderBy](t => t.Name)
-
-            const index = parentFiles.findIndex(t => t.Name[equalsIgnoreCase](pathInfo.Name))
-            info.NextSibling = index >= 0 && index + 1 < parentFiles.length ? parentFiles[index + 1] : undefined
-            info.PreviousSibling = index > 0 ? parentFiles[index - 1] : undefined
-            return info
-        },
-
-        async GetFile(req: PathRequest): Promise<File> {
-            const path = req.Path
-            if (!path) return /*new File*/ { IsFolder: true, Path: "", Name: "Home" }
-            const absPath = new PathInfo(path).ToAbsolute()
-            if (await absPath.IsFile) {
-                return ToFile(await FileInfo.create(absPath.Value))
-            } else if ((await absPath.IsDirectory) || absPath.IsRoot) {
-                return ToFile(await FileInfo.create(absPath.Value))
-            }
-            return null!
-        },
-
-        async Execute(req: PathRequest) {
-            const filename = req.Path
-            const p = await open(filename)
-        },
-
-        Explore(req: PathRequest): void {
-            const cmd = os.platform() === "darwin" ? "open" : "explorer"
-            child_process.exec(`${cmd} ${quote(req.Path)}`)
-        },
-
-        async Delete(req: PathRequest): Promise<void> {
-            const path = req.Path
-            if (await IoFile.Exists(path)) {
-                await IoFile.Delete(path)
-                return
-            }
-            if (await IoDir.Exists(path)) {
-                if (path.split("\\").length <= 2)
-                    throw new Error(
-                        "Delete protection, cannot delete path so short, should be at least depth of 3 levels or more"
-                    )
-                //IoDir.Delete(path, true);
-                await rimraf2(path, { glob: false })
-            }
-        },
-
-        async trash(req: PathRequest): Promise<void> {
-            console.log("Trash 1")
-
-            let path = req.Path
-            await trash([path])
-        },
+export const ListFiles: C.FileService["ListFiles"] = async req => {
+    //if (req.Path == null) {
+    //    return {
+    //        Relatives: { ParentFolder: null, NextSibling: null, PreviousSibling: null },
+    //        File: { Name: "", IsFolder: true },
+    //        Files: GetHomeFiles(),
+    //    };
+    //}
+    if (req.Path != null && req.Path.endsWith(":")) {
+        req.Path += "\\"
     }
-    return _this
+    let res: ListFilesResponse = {
+        Relatives: await GetFileRelatives(req.Path!),
+        File: await GetFile({ Path: req.Path! }),
+        // Files: null,
+    }
+    if (res.File!.IsFolder) {
+        res.Files = await GetFiles(req) //.toArray();
+    }
+    return res
+}
+
+export const GetFiles: C.FileService["GetFiles"] = async req => {
+    if (req.HideFiles && req.HideFolders) return []
+    //else if (!req.MixFilesAndFolders && !req.HideFiles && !req.HideFolders && !req.IsRecursive) {
+    //   const folders = GetFileAndOrFolders(req.Path, req.SearchPattern, req.IsRecursive, false, true);
+    //   const files = GetFileAndOrFolders(req.Path, req.SearchPattern, req.IsRecursive, true, false);
+    //    folders = ApplyRequest(folders, req);
+    //    files = ApplyRequest(files, req);
+    //   const all = folders.concat(files);
+    //    all = ApplyPaging(all, req);
+    //    all = ApplyCaching(all);
+    //    return all;
+    //}
+    //else {
+    let files = await GetFileAndOrFolders({
+        path: req.Path!,
+        searchPattern: req.SearchPattern,
+        recursive: req.IsRecursive,
+        files: !req.HideFiles,
+        folders: !req.HideFolders,
+    })
+    files = await ApplyRequest(files, req)
+    files = ApplyPaging(files, req)
+    files = ApplyCaching(files)
+    return files
+    //}
+}
+
+export const GetFileRelatives: C.FileService["GetFileRelatives"] = async path => {
+    if (!path) return {}
+    const pathInfo = new PathInfo(path)
+    const info: FileRelativesInfo = {}
+    info.ParentFolder = await GetFile({ Path: pathInfo.ParentPath.Value })
+    let xxx = await GetFileAndOrFolders({ path: info.ParentFolder.Path!, files: false, folders: true })
+    const parentFiles = xxx.filter(t => t.IsFolder)[orderBy](t => t.Name)
+
+    const index = parentFiles.findIndex(t => t.Name[equalsIgnoreCase](pathInfo.Name))
+    info.NextSibling = index >= 0 && index + 1 < parentFiles.length ? parentFiles[index + 1] : undefined
+    info.PreviousSibling = index > 0 ? parentFiles[index - 1] : undefined
+    return info
+}
+
+export const GetFile: C.FileService["GetFile"] = async req => {
+    const path = req.Path
+    if (!path) return /*new File*/ { IsFolder: true, Path: "", Name: "Home" }
+    const absPath = new PathInfo(path).ToAbsolute()
+    if (await absPath.IsFile) {
+        return ToFile(await FileInfo.create(absPath.Value))
+    } else if ((await absPath.IsDirectory) || absPath.IsRoot) {
+        return ToFile(await FileInfo.create(absPath.Value))
+    }
+    return null!
+}
+
+export const Execute: C.FileService["Execute"] = async req => {
+    const filename = req.Path
+    const p = await open(filename)
+}
+
+export const Explore: C.FileService["Explore"] = async req => {
+    const cmd = os.platform() === "darwin" ? "open" : "explorer"
+    child_process.exec(`${cmd} ${quote(req.Path)}`)
+}
+
+export const Delete: C.FileService["Delete"] = async req => {
+    const path = req.Path
+    if (await IoFile.Exists(path)) {
+        await IoFile.Delete(path)
+        return
+    }
+    if (await IoDir.Exists(path)) {
+        if (path.split("\\").length <= 2)
+            throw new Error(
+                "Delete protection, cannot delete path so short, should be at least depth of 3 levels or more"
+            )
+        //IoDir.Delete(path, true);
+        await rimraf2(path, { glob: false })
+    }
+}
+
+export const trash: C.FileService["trash"] = async req => {
+    console.log("Trash 1")
+
+    let path = req.Path
+    await trash2([path])
 }
--- server/src/createFileService.ts
+++ server/src/createFileService.ts
@@ -1,15 +1,8 @@
 import * as C from "../../shared/src/contracts"
-import { createFileService2 } from "./FileService"
 import { LevelDb } from "./LevelDb"
+import { ListFiles, GetFiles, GetFileRelatives, GetFile, Execute, Explore, Delete, trash } from "./FileService"
 
 export function createFileService(db: LevelDb): C.FileService {
-    const x = createFileService2()
-    const obj = x as any
-    Object.keys(x.constructor.prototype)
-        .filter(t => typeof obj[t] == "function")
-        .forEach(t => (obj[t] = obj[t].bind(x)))
-
-    const { ListFiles, GetFiles, GetFileRelatives, GetFile, Execute, Explore, Delete, trash } = x
     return {
         async getAllFilesMetadata() {
             return db.getAll("files")
