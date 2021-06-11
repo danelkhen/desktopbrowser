5cb97a621a6d0f88c681aa925d60ccac512eb5a3
Dan-el Khen <danelkhen@gmail.com>
2021-06-11 13:38:42 +0300

    remove tmdb

--- tmdb/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./lib/TmdbProxy"
-export * from "./lib/TmdbScheduler"
-export * from "./lib/v3/TmdbClientV3"
-export * from "./lib/v4/TmdbClientV4"
-export * from "./lib/v4/TmdbV4Proxy"
-import * as TmdbApiV3 from "./lib/tmdb-v3"
-import * as TmdbApiV4 from "./lib/tmdb-v4"
-export { TmdbApiV3, TmdbApiV4 }
