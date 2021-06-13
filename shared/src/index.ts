a9cd25ce027edc912f337840ccf76b7e122f063a
Dan-el Khen <danelkhen@gmail.com>
2021-06-13 17:40:24 +0300

    cleanups

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,4 +1,3 @@
 export * from "./utils"
 export * from "./Array"
 export * from "./String"
-export * from "./Set"
--- shared/src/utils.ts
+++ shared/src/utils.ts
@@ -9,65 +9,8 @@ export function tryParseInt(s: string | null | undefined): number | null {
     return x
 }
 
-export function promiseReuseIfStillRunning<T>(action: () => Promise<T>): () => Promise<T> {
-    let promise: Promise<T> | null = null
-    return function (this: any) {
-        console.log("promiseReuseIfStillRunning", { this: this })
-        if (promise != null) return promise
-        promise = action.call(this).then((t: any) => {
-            promise = null
-            return t
-        })
-        return promise
-    }
-}
-
-/** Will wrap the function and cause it to be reused if it's still running, use for long running async operations that should never run in parallel */
-export function ReusePromiseIfStillRunning(): MethodDecorator {
-    return function (target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
-        const originalFuncKey = "_" + String(propertyKey) + "_original"
-        const promiseKey = "_" + String(propertyKey) + "_promise"
-        target[originalFuncKey] = descriptor.value
-        descriptor.value = new Function(`// generated code
-if (this.${promiseKey} == null) {
-    this.${promiseKey} = this.${originalFuncKey}().then(t => { 
-        this.${promiseKey} = null; 
-        return t;
-    }, 
-    err => {
-        this.${promiseKey} = null; 
-        return Promise.reject(err); 
-    });
-}
-return this.${promiseKey};
-`)
-    }
-}
-
-/** Will wrap the function and cause it to run only once, regardless of how many times it was called, use for things like init() */
-export function ReusePromise(): MethodDecorator {
-    return function (target: any, propertyKey: string | Symbol, descriptor: PropertyDescriptor) {
-        const originalFuncKey = "_" + String(propertyKey) + "_original"
-        const promiseKey = "_" + String(propertyKey) + "_promise"
-        target[originalFuncKey] = descriptor.value
-        descriptor.value = new Function(`// generated code
-if (this.${promiseKey} == null)
-    this.${promiseKey} = this.${originalFuncKey}();
-return this.${promiseKey};
-`)
-    }
-}
-
 export function sleep(ms: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, ms || 0))
-}
-
-export interface SelectorFunc<T, R> {
-    (obj: T): R
-}
-
-export interface ComparerFunc<T> {
-    (x: T, y: T): number
 }
 
 export function tryParseExactMoment(s: string, format: string): Moment | null {
@@ -103,11 +46,3 @@ export function toFriendlyRelative2(dt2: Date | null, rel2?: Date): string {
     }
     return dt.format("YYYY-MM-DD")
 }
-
-export type ExtractKeysOfType<T extends object, U> = {
-    [K in keyof T]: T[K] extends U ? K : never
-}[keyof T]
-
-export type FilterKeysOfType<T extends object, U> = Pick<T, ExtractKeysOfType<T, U>>
-
-export type AllowOnly<T, U> = Record<keyof T, U>
